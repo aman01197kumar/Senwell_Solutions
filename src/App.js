31fe8c16aa6017a634362038f0ebcab4177bae40
Aman <aman01197kumar@gmail.com>
2023-05-26 14:42:50 +0530

    added tables and desired solutions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data.json";
 
-function App() {
+
+export default function App() {
+  const [tableData, setTableData] = React.useState({});
+
+  const getRowColor = (count) => {
+    if (count <= 2) {
+      return "#f8d7da";
+    } else if (count >= 3 && count <= 9) {
+      return "#fff3cd";
+    } else {
+      return "#d4edda";
+    }
+  };
+
+  function countOccurrences(names) {
+    const nameCounts = {};
+    names.forEach((name) => {
+      if (name in nameCounts) {
+        nameCounts[name]++;
+      } else {
+        nameCounts[name] = 1;
+      }
+    });
+    return nameCounts;
+  }
+
+  React.useEffect(() => {
+    const nameCounts = countOccurrences(data?.name);
+    setTableData(nameCounts);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(tableData).map(([name, count]) => (
+            <tr key={name} style={{ background: getRowColor(count) }}>
+              <td>{name}</td>
+              <td>{count}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
